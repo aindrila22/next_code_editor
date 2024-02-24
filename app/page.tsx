8e5b1bc5a3d25ac3576627591e7384f6297ffe96
aindrila22 <aindrilabhattacharjee72@gmail.com>
2024-02-24 21:46:02 +0530

    tested code

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-
-import NotesPicker from "@/components/NotePicker";
-import Notes from "@/components/Notes";
+import NotesPicker from '@/components/NotePicker'
+import Notes from '@/components/Notes'
 
 export default function Home() {
   return (
-    <main>
+    <main className="bg-slate-950 w-full min-h-screen pb-10">
       <NotesPicker />
       <Notes />
     </main>
-  );
+  )
 }
--- components/NotePicker.tsx
+++ components/NotePicker.tsx
@@ -1,33 +1,33 @@
-"use client";
+'use client'
 
-import React, { useState } from "react";
-import Tiptap from "./Tiptap";
-import { v4 as uuidv4 } from "uuid";
+import React, { useState } from 'react'
+import Tiptap from './Tiptap'
+import { v4 as uuidv4 } from 'uuid'
 
 const Todo = () => {
-  const [content, setContent] = useState<string>("");
+  const [content, setContent] = useState<string>('')
   const handleContentChange = (reason: any) => {
-    setContent(reason);
-  };
+    setContent(reason)
+  }
   const handleSubmit = (e: any) => {
-    e.preventDefault();
+    e.preventDefault()
     const data = {
       id: uuidv4(),
       content: content,
-    };
-    console.log(data);
-    const existingDataString = localStorage.getItem("myData");
+    }
+    console.log(data)
+    const existingDataString = localStorage.getItem('myData')
     const existingData = existingDataString
       ? JSON.parse(existingDataString)
-      : [];
-    const updatedData = [...existingData, data];
-    localStorage.setItem("myData", JSON.stringify(updatedData));
-    setContent("");
-  };
+      : []
+    const updatedData = [...existingData, data]
+    localStorage.setItem('myData', JSON.stringify(updatedData))
+    setContent('')
+  }
   return (
     <form
       onSubmit={handleSubmit}
-      className="max-w-3xl w-full grid place-items-center mx-auto my-10"
+      className="max-w-3xl w-full grid place-items-center mx-auto pt-10 mb-10"
     >
       <div className="text-3xl text-center text-sky-300 mb-10">
         Notes Picker
@@ -37,7 +37,7 @@ const Todo = () => {
         onChange={(newContent: string) => handleContentChange(newContent)}
       />
     </form>
-  );
-};
+  )
+}
 
-export default Todo;
+export default Todo
